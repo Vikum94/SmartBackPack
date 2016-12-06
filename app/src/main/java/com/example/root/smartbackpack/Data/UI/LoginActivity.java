c361eec84e5d7c6c60b79ac5fbb585e87f066c2a
Vikum Bandara <vikumbandara@hotmail.com>
2016-12-06 22:45:01 +0530

    Added folders UI and Control

--- app/src/main/java/com/example/root/smartbackpack/Data/UI/LoginActivity.java
+++ app/src/main/java/com/example/root/smartbackpack/Data/UI/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.example.root.smartbackpack;
+package com.example.root.smartbackpack.Data.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.root.smartbackpack.R;
 
 public class LoginActivity extends AppCompatActivity {
 
