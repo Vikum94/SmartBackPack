88243d6b625e870e138f6f35163484bbba6b5edd
Vikum Bandara <vikumbandara@hotmail.com>
2016-12-07 00:37:11 +0530

    Methods and Attributes in Bag calss

--- app/src/main/java/com/example/root/smartbackpack/Data/Model/Bag.java
+++ app/src/main/java/com/example/root/smartbackpack/Data/Model/Bag.java
@@ -1,11 +1,44 @@
 package com.example.root.smartbackpack.Data.Model;
 
+import java.net.PasswordAuthentication;
+import java.util.Date;
+
 /**
  * Created by root on 12/6/16.
  */
 
 public class Bag {
     private String bagName;
-    private String
+    private String wifiPassword;
+    private String location;
+    private Date dateTime;
+    private PasswordAuthentication pass;
+
+    public Bag(String bagName, String wifiPassword) {
+        this.bagName = bagName;
+        this.wifiPassword = wifiPassword;
+    }
+
+    public String getBagName() {
+        return bagName;
+    }
+    public void setBagName(String bagName) {
+        this.bagName = bagName;
+    }
+
+    public String getWifiPassword() {
+        return wifiPassword;
+    }
+    public void setWifiPassword(String wifiPassword) {
+        this.wifiPassword = wifiPassword;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
 
 }
