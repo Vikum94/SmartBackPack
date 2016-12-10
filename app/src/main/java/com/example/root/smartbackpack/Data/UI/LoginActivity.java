4aa65b0b2854f85c4fefe66aba232b6dd0ccfb5e
Vikum Bandara <vikumbandara@hotmail.com>
2016-12-10 22:03:00 +0530

    Login Main Signup activities

--- app/src/main/java/com/example/root/smartbackpack/Data/UI/LoginActivity.java
+++ app/src/main/java/com/example/root/smartbackpack/Data/UI/LoginActivity.java
@@ -1,15 +1,38 @@
 package com.example.root.smartbackpack.Data.UI;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.root.smartbackpack.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button signin;
+    Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signin = (Button) findViewById(R.id.signIn);
+        signup = (Button) findViewById(R.id.signUp);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/root/smartbackpack/Data/UI/SignupActivity.java
+++ app/src/main/java/com/example/root/smartbackpack/Data/UI/SignupActivity.java
@@ -5,12 +5,11 @@ import android.os.Bundle;
 
 import com.example.root.smartbackpack.R;
 
-public class MainActivity extends AppCompatActivity {
-
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
     }
 }
