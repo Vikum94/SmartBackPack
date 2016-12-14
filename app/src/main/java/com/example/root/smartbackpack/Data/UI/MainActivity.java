faa37463e80973994302736f67f65911d0c6916e
Vikum Bandara <vikumbandara@hotmail.com>
2016-12-15 01:05:09 +0530

    New code for main activity xml

--- app/src/main/java/com/example/root/smartbackpack/Data/UI/MainActivity.java
+++ app/src/main/java/com/example/root/smartbackpack/Data/UI/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.root.smartbackpack.Data.UI;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Switch;
 
 import com.example.root.smartbackpack.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> adapter;
+    private Switch aSwitch;
+    private String bagName;
 
-
+    public void addBagByName(String bagName){
+        this.bagName=bagName;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.selectBag);
+        String items[] = {};
+        arrayList = new ArrayList<>(Arrays.asList(items));
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.bag, arrayList);
+        listView.setAdapter(adapter);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addBag);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                arrayList.add(bagName);
+                adapter.notifyDataSetChanged();
+                Intent intent = new Intent(MainActivity.this, AddBagActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
